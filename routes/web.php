c24b206ff92bc60693cd0feff4b90b6b5988f537
LynlynDev <reinselynder@gmail.com>
2023-04-18 13:21:45 +0200

    Changement

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use app\Http\Controllers\Regioncontroller;
+use App\Http\Controllers\RegionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,20 +17,22 @@ use app\Http\Controllers\Regioncontroller;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/region-create', function () { //le chemin d'accès qui s'affichera sur l'URL
+Route::get('/region_create', function () { //le chemin d'accès qui s'affichera sur l'URL
     return view('formulaire_region'); //le nom du fichier blade dans lequel la route doit nous rediriger
 });
 
-
-Route::get("/region_create", [RegionController::class, "index"]);
-Route::post("/region_store", [RegionController::class, "store"]);
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-
-Route::post('/region_insert', function () {
+Route::post('/region_insert', function () { //le chemin d'accès qui s'affichera sur l'URL
+    return view('formulaire_region'); //le nom du fichier blade dans lequel la route doit nous rediriger
     return "Nous avons bien reçu votre région qui est : " . request('region');
 });
 
-//Route::get("/region_create" , [Regioncontroller::class,"index"]);
-//Route::get("/region_insert" , [Regioncontroller::class,"show"]);
+Route::get("/region_create", [RegionController::class, "formulaire_region"]);
+Route::post("/region_insert", [RegionController::class, "store"]);
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+// Route::post('/region_insert', function () {
+//     return "Nous avons bien reçu votre région qui est : " . request('region');
+// });
+
