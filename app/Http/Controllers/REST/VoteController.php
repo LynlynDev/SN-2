e7a0d9d5a6d2d2077ec18533cf70e69d1935914b
LynlynDev <reinselynder@gmail.com>
2023-05-11 00:12:32 +0200

    fonction resultat

--- app/Http/Controllers/REST/VoteController.php
+++ app/Http/Controllers/REST/VoteController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\REST;
 use App\Http\Controllers\Controller;
 use App\vote;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Throwable;
 
 class VoteController extends Controller
 {
@@ -14,14 +16,8 @@ class VoteController extends Controller
     public function index()
     {
         //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        $vote = Vote::all();
+        return response()->json($vote, 200);
     }
 
     /**
@@ -30,6 +26,29 @@ class VoteController extends Controller
     public function store(Request $request)
     {
         //
+        $this->validate(
+            $request,
+            [
+                'date' => 'required',
+                'id_election' => 'required|max:100',
+                'id_bulletin' => 'required|max:5',
+                'id_participant' => 'required|max:2',
+            ]
+        );
+
+        try {
+            DB::beginTransaction();
+            $vote = vote::create([
+                'date' => $request->date,
+                'id_election' => $request->id_election,
+                'id_bulletin' => $request->id_bulletin,
+                'id_participant' => $request->id_participant,
+            ]);
+            DB::commit();
+            return response()->json($vote, 201);
+        } catch (\Throwable $th) {
+            return response()->json("{'error: Imposible de sauvegarder le vote'}", 404);
+        }
     }
 
     /**
@@ -41,26 +60,33 @@ class VoteController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(vote $vote)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, vote $vote)
+    public function update(Request $request, $id)
     {
         //
+        try {
+            $vote = vote::find($id);
+            $vote->update($request->all());
+            response()->json("{'Modification réussie du vote'}", 200);
+            return $vote;
+        } catch (\Throwable $error) {
+            return response()->json("{'error: Imposible de mettre a jour le vote'}", 404);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(vote $vote)
+    public function destroy($id)
     {
         //
+        try {
+            $vote = vote::find($id);
+            $vote->delete();
+            return response()->json("{'Suppresion réussie du vote'}", 200);
+        } catch (\Throwable $error) {
+            return response()->json("{'error: Imposible de supprimé le vote'}", 404);
+        }
     }
 }
